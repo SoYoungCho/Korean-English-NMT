50d796c603cc527066216d1917852762a8b80019
Soyoung Cho <38921656+SoYoungCho@users.noreply.github.com>
2020-02-06 10:15:02 +0900

    Create simple_attention_comments.py

---
+++ simple_attention_comments.py
@@ -0,0 +1,79 @@
+"""
+
+Copyright 2017- IBM Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Attention(nn.Module):
+    r"""
+    Applies an attention mechanism on the output features from the decoder.
+
+    .. math::
+            \begin{array}{ll}
+            x = context*output \\
+            attn = exp(x_i) / sum_j exp(x_j) \\
+            output = \tanh(w * (attn * encoder_output) + b * output)
+            \end{array}
+
+    Args:
+        dim(int): The number of expected features in the output
+
+    Inputs: decoder_output, encoder_output
+        - **decoder_output** (batch, output_len, hidden_size): tensor containing the output features from the decoder.
+        - **encoder_output** (batch, input_len, hidden_size): tensor containing features of the encoded input sequence.
+
+    Outputs: output, attn
+        - **output** (batch, output_len, dimensions): tensor containing the attended output features from the decoder.
+        - **attn** (batch, output_len, input_len): tensor containing attention weights.
+
+    Attributes:
+        linear_out (torch.nn.Linear): applies a linear transformation to the incoming data: :math:`y = Ax + b`.
+        mask (torch.Tensor, optional): applies a :math:`-inf` to the indices specified in the `Tensor`.
+
+    Examples::
+
+         >>> attention = seq2seq.models.Attention(256)
+         >>> encoder_output = Variable(torch.randn(5, 3, 256))
+         >>> output = Variable(torch.randn(5, 5, 256))
+         >>> output, attn = attention(output, encoder_output)
+
+    """
+    def __init__(self, dim):
+        super(Attention, self).__init__()
+        self.linear_out = nn.Linear(dim*2, dim)
+
+    def forward(self, decoder_output, encoder_output):
+        batch_size = decoder_output.size(0)   # batch
+        hidden_size = decoder_output.size(2)  # hidden_size
+        input_size = encoder_output.size(1)
+
+        # get attention score
+        attn = torch.bmm(decoder_output, encoder_output.transpose(1, 2)) # bmm : Batch Matrix Multiplication
+        # get attention distribution
+        attn = F.softmax(attn.view(-1, input_size), dim=1).view(batch_size, -1, input_size)
+        # get attention value
+        attn_val = torch.bmm(attn, encoder_output) # get attention value
+
+        # concat -> (batch, o1ut_len, 2*dim)
+        combined = torch.cat((attn_val, decoder_output), dim=2)
+        # output -> (batch, out_len, dim)
+        output = torch.tanh(self.linear_out(combined.view(-1, 2 * hidden_size))).view(batch_size, -1, hidden_size)
+
+        return output, attn
